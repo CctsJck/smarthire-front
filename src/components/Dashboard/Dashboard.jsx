d46b661d825ced40a82b0d1d35047ec2973491ed
Benjamin <bouillinbenjamin@gmail.com>
2023-09-23 19:14:21 -0300

    dashboard avances

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -8,8 +8,26 @@ export const Dashboard = () => {
   const [busqueda, setBusqueda] = useState({});
   const [bandera, setBandera] = useState(false);
   const [resultados, setResultados] = useState([]);
+  const [order, setOrder] = useState("ASC")
+  const sorting = (col) => {
+    if (order === "ASC") {
+      const sorted = [...resultados].sort((a, b) =>
+        a[col].toString().toLowerCase() > b[col].toString().toLowerCase() ? 1 : -1
+      );
+      setResultados(sorted);
+      setOrder("DSC");
+    }
+    if (order === "DSC") {
+      const sorted = [...resultados].sort((a, b) =>
+        a[col].toString().toLowerCase() < b[col].toString().toLowerCase() ? 1 : -1
+      );
+      setResultados(sorted);
+      setOrder("ASC");
+    }
+  };
 
   function elegirPregunta() {
+    setResultados([]);
     setShowModal(true);
   }
 
@@ -46,7 +64,7 @@ export const Dashboard = () => {
 
     let config = {
       method: "get",
-      url: `http://localhost:5000/result/filter/10`,
+      url: `http://localhost:5000/result/filter/${pregunta.id}`,
       headers: {
         Authorization: `Bearer ${sessionStorage.getItem("token")}`,
       },
@@ -63,25 +81,30 @@ export const Dashboard = () => {
           navigate("/login");
         }
       });
-
   }
 
   return (
     <>
-      <div className="col-md-6 d-flex justify-content-end mx-5 mt-5">
-        <h1>{busqueda.name}</h1>
+      <div className="d-flex justify-content-start align-items-center mx-5 mt-4">
+        <div className="col-md-10 d-flex mx-5 mt-4">
+          <h1 class="mx-5">{busqueda.name}</h1>
+        </div>
       </div>
       <div className="d-flex justify-content-center align-items-center mx-2">
         <div className="col-md-10 d-flex card">
           <h3 className="d-flex justify-content-center my-4">
-            Elegir pregunta la pregunta a analizar, para ver las respuestas de los candidatos
+            Elegir pregunta la pregunta a analizar, para ver las respuestas de
+            los candidatos
           </h3>
           <div class="d-flex justify-content-center align-items-center">
             <div class="col-md-3 d-flex mb-4">
               <button
                 type="button"
                 className="btn btn-primary custom-btn mx-5 my-3"
-                onClick={elegirPregunta}>Busca la pregunta que quieras analizar</button>
+                onClick={elegirPregunta}
+              >
+                Busca la pregunta que quieras analizar
+              </button>
             </div>
           </div>
         </div>
@@ -97,6 +120,42 @@ export const Dashboard = () => {
       ) : (
         <></>
       )}
+
+      <div className="tabla-resultados">
+        {
+          resultados.length > 0 ? (
+            <div className="d-flex justify-content-center align-items-center mx-2">
+              <div className="col-md-10 d-flex mt-3 card">
+                <table>
+                  <thead>
+                    <th onClick={()=>sorting("name")}>Candidato</th>
+                    <th onClick={()=>sorting("happy")}>Felicidad</th>
+                    <th onClick={()=>sorting("angry")}>Enojo</th>
+                    <th onClick={()=>sorting("disgust")}>Disgusto</th>
+                    <th onClick={()=>sorting("fear")}>Miedo</th>
+                    <th onClick={()=>sorting("sad")}>Tristeza</th>
+                    <th onClick={()=>sorting("surprise")}>Sorpresa</th>
+                  </thead>
+                  <tbody>
+                    {resultados.map((result)=>(
+                        <tr key={result.id}>
+                            <td>{result.candidate.name+' '+result.candidate.surename}</td>
+                            <td>{result.happy}</td>
+                            <td>{result.angry}</td>
+                            <td>{result.disgust}</td>
+                            <td>{result.fear}</td>
+                            <td>{result.sad}</td>
+                            <td>{result.surprise}</td>
+                        </tr>
+
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ) : null /* Agrega una parte para el caso contrario si es necesario */
+        }
+      </div>
     </>
   );
 };
