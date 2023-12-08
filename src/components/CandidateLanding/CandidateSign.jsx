a50a13b76342c3501f3113ba521267bdd720c346
CctsJck <julipro99@gmail.com>
2023-12-08 19:19:17 -0300

    cambio generales

--- src/components/CandidateLanding/CandidateSign.jsx
+++ src/components/CandidateLanding/CandidateSign.jsx
@@ -5,6 +5,9 @@ import axios from 'axios';
 import CryptoJS from 'crypto-js';
 
 export const CandidateSign = () =>{
+  
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
     const [nombre,setNombre] = useState('')
     const [apellido,setApellido] = useState('')
     const [file,setFile] = useState(null);
@@ -117,13 +120,31 @@ export const CandidateSign = () =>{
                     />
                     </div>
                     <label for="formFile" class="form-label">Ingrese su CV de LinkedIn</label>
-                    <div class='mb-3 col-md-12 d-flex'>
+                    <div class='mb-3 col-md-11 d-flex'>
                         <div class = 'col-md-12 d-flex justify-content-start'>
                             <input class="form-control" type="file" id="formFile" name="file"
                             onChange={(e) => setFile(e.target.files[0])}
                             required></input>
                         </div>                
                     </div>
+                    <div class="col-md-1 d-flex justify-content-start ms-2"
+              >
+                
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-info-circle"
+                  viewBox="0 0 16 16"
+                  data-bs-toggle="tooltip" 
+                  data-bs-title="Para descargarlo, en su perfil de LinkedIn, haciendo clic en MAS se puede descargar en formato .pdf"
+                  
+                >
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                  <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
+                </svg>
+              </div>
                     <div class='d-flex justify-content-center'>
                     <button type="submit" onClick={handleSubmit} className="btn btn-primary mb-4 ">
                         ¡Comenzar!
